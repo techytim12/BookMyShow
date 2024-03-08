94c5846e9e1dd1dc7a172716a0b2b1a4d640e928
Thomas Philip <thomasphilipchry@gmail.com>
2024-03-08 23:27:11 +0530

    Initial updates for BookMyShow

---
+++ src/main/java/com/scaler/BookMyShow/model/BaseModel.java
@@ -0,0 +1,26 @@
+package com.scaler.BookMyShow.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public abstract class BaseModel {
+
+    @Id // -> tells the attribute will be PRIMARY_KEY
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private int id;
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
+}
